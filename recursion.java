92e65d517183dfc299147f58af3b739fba394e7c
Jude Rizzo <jrizzo00@stuy.edu>
2019-01-31 14:39:11 -0500

    fibbonaci

--- recursion.java
+++ recursion.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class recursion{
     /*You may write additional private methods */
 
@@ -8,11 +9,12 @@ public class recursion{
     */
 
     public static double sHelper(double n, double tolerance, double ans){
-      
+      if(n - ans * ans > (n * tolerance)/100) return ans;
+      return recursion.sHelper(n, tolerance, (ans + (n / ans))/2 );
     }
 
     public static double sqrt(double n, double tolerance){
-      return recursion.sHelper(n, toleance, 0);
+      return recursion.sHelper(n, tolerance, 0);
 
     }
 
